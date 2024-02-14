a53bceb0346e890ac8c554b3d87471578c614573
derya köse <derkose@hotmail.com>
2024-02-14 15:51:01 +0300

    0

---
+++ DataAccess/Concretes/Repositories/BrandRepository.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Abstracts;
+using DataAccess.Concretes.EntityFramework.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes.Repositories;
+
+public class BrandRepository : IBrandRepository
+{
+    private BaseDbContext _baseDbContext;
+
+    public BrandRepository(BaseDbContext baseDbContext)
+    {
+        _baseDbContext = baseDbContext;
+    }
+
+    public void Add(Brand brand);
+}
