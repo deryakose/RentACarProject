aff74f29ab9d859faed70a4b5e9ebb71435e3ab2
derya köse <derkose@hotmail.com>
2024-02-21 13:39:19 +0300

    1

---
+++ Business/Abstracts/IModelService.cs
@@ -0,0 +1,14 @@
+﻿using Business.Requests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts;
+
+public class IModelService
+{
+    Task<CreateModelResponce> AddAsync(CreateModelRequest request);
+    Task<List<GetAllModelResponce>>
+}
